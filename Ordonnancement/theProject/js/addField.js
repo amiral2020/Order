becffd44be7e6b1ae487c6266da378c38443ef88
Mohamed BELMAHI <medbelmahi@outlook.com>
2014-11-09 14:43:35 +0000

    last commit

--- Ordonnancement/theProject/js/addField.js
+++ Ordonnancement/theProject/js/addField.js
@@ -1,3 +1,5 @@
+var theIProcessus = 0;
+
 $(function()
 {
     $(document).on('click', '.btn-add', function(e)
@@ -13,10 +15,12 @@ $(function()
             .removeClass('btn-add').addClass('btn-remove')
             .removeClass('btn-success').addClass('btn-danger')
             .html('<span class="glyphicon glyphicon-minus"></span>');
+        theIProcessus ++;
+        $('.processuName:last').html("P"+theIProcessus);
     }).on('click', '.btn-remove', function(e)
     {
     	$(this).parents('.entry:last').remove();
-
+    	theIProcessus --;
 		e.preventDefault();
 		return false;
 	});
