98a6fd2211bc77f0f218a76e8cec6045056a8b33
Integrale <Integrale@Integrale-PC>
2014-11-11 04:01:14 +0000

    algo :3

--- Ordonnancement/theProject/js/ordonnancement.js
+++ Ordonnancement/theProject/js/ordonnancement.js
@@ -16,7 +16,45 @@ function algorithmes(){
 		console.log("name : "+donnee[i].name+"\tcoming : "+donnee[i].coming+"\ttime : "+donnee[i].time);
 	}
 	
+	// impl. des algorithmes
 	
+	function fifo(donnee) {
+		
+		return donnee;
+	}
+	
+	function pcter() {
+		
+		return donnee.sort();
+	}
+
+		
+	function pcte(donnee) {
+		
+		return donnee.reverse();
+	}
+	
+	
+	function tourniquet(donnee) {
+		
+	//	var quantum = 1;
+		var tempDonnee = Array();
+		
+		for(i; i<donnee.length;i++){
+			
+			if(donnee[i].time>1){
+				
+				donnee[i].time-= 1;
+				tempDonnee[i] = donnee[i];
+			
+			}else{
+				
+				tempDonnee[i] = donnee[i];
+				donnee.splice(donnee.indexOf(value), i); // make sur we delete it like this :3
+			}
+		}
+		return tempDonnee;
+		}
 	
 }
 
